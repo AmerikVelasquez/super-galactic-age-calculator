be7aeca1e64ae8b2be31e9a60c36085c4d7bbbe4
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-30 16:45:48 -0700

    add good pass to venus constructor

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -4,6 +4,6 @@ describe('Venus', () => {
   
   test('should correctly change a persons age to the age in venus years', () => {
     const venus1 = new Venus(31)
-    expect(venus1.age).toEqual(50);
+    expect(venus1.convert()).toEqual(50);
   })
 })
--- src/venus.js
+++ src/venus.js
@@ -1,3 +1,11 @@
 export default class Venus {
+  constructor(age) {
+    this.age= age;
+  }
 
+  convert(age){
+    const convertAge = Math.floor(this.age / .62);
+    return convertAge;
+  }
 }
+
