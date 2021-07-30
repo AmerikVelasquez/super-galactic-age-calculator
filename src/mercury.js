52ed35d6b1ab89d899f8d73bc4d41df59fd82f3b
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-30 16:16:40 -0700

    add passing test for convert prototype

--- src/mercury.js
+++ src/mercury.js
@@ -1,9 +1,10 @@
 export default class Mercury {
   constructor(age) {
-  
+    this.age= age
   }
 
   convert(age){
-
+    const convertAge = Math.floor(this.age / .24);
+    return convertAge;
   }
 }
