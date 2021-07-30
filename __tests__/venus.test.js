b2e9877aaa2ef2f489d4e27961a9ec07f9d6d46f
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-30 16:42:05 -0700

    add good fail for venus class

---
+++ __tests__/venus.test.js
@@ -0,0 +1,9 @@
+import Venus from './../src/venus.js'
+
+describe('Venus', () => {
+  
+  test('should correctly change a persons age to the age in venus years', () => {
+    const venus1 = new Venus(31)
+    expect(venus1.age).toEqual(50);
+  })
+})
