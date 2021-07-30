e61a671f406fa4870f5f5f17a88e995d7757ddc5
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-30 16:57:29 -0700

    add good pass to mars convert prototype

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -3,7 +3,7 @@ import Mars from './../src/mars.js'
 describe('Mars', () => {
   
   test('should correctly change a persons age to the age in mars years', () => {
-    const mars1 = new Mars(88)
-    expect(mars1.age).toEqual(188);
+    const mars1 = new Mars(10)
+    expect(mars1.convert()).toEqual(18);
   })
 })
--- src/mars.js
+++ src/mars.js
@@ -1,3 +1,10 @@
 export default class Mars {
-  
+  constructor(age) {
+    this.age = age;
+  }
+
+  convert(age) {
+    const convertMarsAge = Math.floor(this.age * 1.88);
+    return convertMarsAge;
+  }
 }
