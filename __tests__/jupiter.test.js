3991f126f8915140c1f64241f447d6a99aa91b11
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-31 09:24:37 -0700

    add good fail to expectation prototype and fix math

--- __tests__/jupiter.test.js
+++ __tests__/jupiter.test.js
@@ -4,6 +4,11 @@ describe('Jupiter', () => {
   
   test('should correctly change a persons age to the age in jupiter years', () => {
     const jupiter1 = new Jupiter(50)
-    expect(jupiter1.convert()).toEqual(593);
+    expect(jupiter1.convert()).toEqual(4);
+  })
+
+  test('should correctly give the life expectancy of someone living on jupiter', () =>{
+    const jupiter2 = new Jupiter(50, "no","yes", "poor")
+    expect(jupiter2.expectation()).toEqual(5)
   })
 })
--- src/jupiter.js
+++ src/jupiter.js
@@ -1,10 +1,17 @@
 export default class Jupiter {
-  constructor(age) {
+  constructor(age, smoke, drink, livingWage) {
     this.age = age;
+    this.smoke = smoke;
+    this.drink = drink;
+    this.livingWage = livingWage;
   }
 
   convert() {
-    const convertJupiterAge = Math.floor(this.age * 11.86);
+    const convertJupiterAge = Math.floor(this.age / 11.86);
     return convertJupiterAge;
+  }
+
+  expectation(){
+
   }
 }
