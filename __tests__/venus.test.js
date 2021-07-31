5d534840fe22ba1992a1ca0f83ca522f7d0c6f3f
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-31 09:12:24 -0700

    add good pass to expectation prototype

--- __tests__/venus.test.js
+++ __tests__/venus.test.js
@@ -6,4 +6,9 @@ describe('Venus', () => {
     const venus1 = new Venus(31)
     expect(venus1.convert()).toEqual(50);
   })
+
+  test('should correctly show life expectancy on venus',() =>{
+    const venus2 = new Venus(31,"no","no","poor")
+    expect(venus2.expectation()).toEqual(112);
+  })
 })
--- src/venus.js
+++ src/venus.js
@@ -1,11 +1,30 @@
 export default class Venus {
-  constructor(age) {
+  constructor(age, smoke, drink, livingWage) {
     this.age= age;
+    this.smoke = smoke;
+    this.drink = drink;
+    this.livingWage = livingWage;
   }
 
   convert(){
     const convertVenusAge = Math.floor(this.age / .62);
     return convertVenusAge;
+  }
+
+  expectation(){
+    let life = 80
+    if (this.livingWage === "poor"){
+      life =Math.floor((life - 10)/.62);
+      return life;
+    }
+    else if (this.smoke === "yes"){
+      life = Math.floor((life- 8)/.62);
+      return life;
+    }
+    else if (this.drink === "yes"){
+      life = Math.floor((life -10)/.62);
+      return life;
+    }
   }
 }
 
